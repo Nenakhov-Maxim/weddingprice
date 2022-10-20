f96e6af5a957c939ce96de7b12645a1cc938d956
Nenakhov.Max@yandex.ru <Nenakhov.Max@yandex.ru>
2022-10-20 22:07:58 +0500

    !!

--- twoapples.py
+++ twoapples.py
@@ -1,7 +1,3 @@
-from flask import Flask
-from config import Config
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 from app import application
 
 from app import routes, models
