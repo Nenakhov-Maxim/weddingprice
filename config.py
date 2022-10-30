0aff58df4223e212783e5ab6df4e90d6c2752106
Nenakhov.Max@yandex.ru <Nenakhov.Max@yandex.ru>
2022-10-30 16:36:33 +0500

    summary

--- config.py
+++ config.py
@@ -2,12 +2,13 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or ''
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'sdfjk7865432wedfgbhntj76543'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                               'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    EMAIL_LOGIN = ''
-    EMAIL_PASSWORD = ''
-    HOST = ""
-    TELEGRAM_chat_id = ""
-    bot_access_token = ""
+    EMAIL_LOGIN = 'order@weddingprice.ru'
+    EMAIL_PASSWORD = '55369100Max'
+    HOST = "server152.hosting.reg.ru"
+    TELEGRAM_chat_id = "-1001838710767"
+    bot_access_token = "5692158136:AAGaP-CReaNsGXlPEwKJHPGKviqd5FhScKI"
+    SEND_TO = ['trg1101@yandex.ru', 'STS_71@mail.ru']
--- twoapples.py
+++ twoapples.py
@@ -1,4 +1,5 @@
 from app import application
+
 from app import routes, models, send_mail, send_telegram
 if __name__ == "__main__":
    application.run(debug=False)
